5a60aae2e3022d4a668e1c565e834174943ea0bc
Patrick Niebrzydowski <patrick.niebrzydowski@gmail.com>
2018-09-09 11:58:01 +0200

    add basic timer functionality

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -1,19 +1,65 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import CountdownItem from './CountdownItem';
 
 class CountdownTimer extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      timer: null,
       currentTime: null,
-      targetTime: null,
+      targetTime: moment('2018-11-23'),
     }
+    this.tick = this.tick.bind(this);
   }
+
+  componentDidMount() {
+    const timer = setInterval(this.tick, 1000);
+    const currentTime = moment();
+
+    this.setState({currentTime, timer});
+  }
+
+  componentWillUnmount() {
+    this.setState({
+      timer: null,
+    });
+  }
+
+  tick() {
+    // could also just subtract a second from previous state, but lagging issues?
+    const currentTime = moment();
+    this.setState({ currentTime });
+  }
+
   render() {
+    const { currentTime, targetTime } = this.state;
+    if (!currentTime || !targetTime) return null;
+    const difference = moment.duration(targetTime.diff(currentTime));
+    const displays = [
+      {
+        label: 'Days',
+        value: difference.days().toString()
+      },
+      {
+        label: 'Hours',
+        value: difference.hours().toString()
+      },
+      {
+        label: 'Minutes',
+        value: difference.minutes().toString()
+      },
+      {
+        label: 'Seconds',
+        value: difference.seconds().toString()
+      }
+    ];
     return (
       <div className="countdown-timer">
         <header>Starts in</header>
-        <CountdownItem label="" value="" /> 
+        {displays.map(display => {
+          return <CountdownItem key={ display.label } label={ display.label } value={ display.value } />
+        })}
       </div>
     );
   }
